16a74808878cebebbb259663154b602939431342
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2022-12-12 13:11:51 +0100

    added form validation

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -4,8 +4,20 @@ import Button from './Button'
 
 function ReviewForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const textChangeHandler = (e) => {
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if (text !== '' && text.trim().length <=15){
+            setMessage('input text must be above 15 characters')
+            setBtnDisabled(true)
+        }else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -15,8 +27,9 @@ function ReviewForm() {
             <h4>Kindly Review Our Service You Just Experienced!</h4>
             <div className="input-group">
                 <input onChange={textChangeHandler} type="text" placeholder="write us a review" value={text} />
-                <Button type="submit">Submit</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Submit</Button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
