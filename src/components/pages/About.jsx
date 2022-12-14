6de9ffabb5b666d538f1ea11f1c3b52f1bdfdfcd
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2022-12-14 11:11:17 +0100

    update the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -7,8 +7,9 @@ function About() {
             <Card>
                     <h3 className="head">About The Review Application</h3>
                     <p>This is an application for a customer to give a review for a product usage or for a service.</p>
-
+                      <br />
                     <p>Version: 1.0.0</p>
+                    <br />
                     <p>
                       <Link to='/'>Go Back Home</Link>
                     </p>
