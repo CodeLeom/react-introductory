36d1fefdb6be47798e5ee58ebe8f4c3c2657fd19
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2022-12-14 13:24:13 +0100

    added two more reviews

--- src/components/data/reviewData.js
+++ src/components/data/reviewData.js
@@ -18,7 +18,17 @@ const reviewData = [
         id: 4,
         rating: 4,
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
-      },
+      },   
+      {
+        id: 5,
+        text: 'Accumsan justo quisque curabitur dis iaculis facilisi erat nasc.',
+        rating: 10
+    },
+    {
+        id: 6,
+        text: 'Duis lectus montes congue nibh facilisis nisi habitant primis',
+        rating: 9
+    },
 
 ]
 
